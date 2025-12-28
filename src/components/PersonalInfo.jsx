97853c326480a96599c8919d7190145bf2bd0830
Your Name <mugeta88@gmail.com>
2025-12-28 08:51:13 -0500

    created a PersonalInfo component to hold name, title, and buttons for email and linkedin accounts

---
+++ src/components/PersonalInfo.jsx
@@ -0,0 +1,15 @@
+function PersonalInfo() {
+    return (
+        <main>
+            <img src="src/assets/profile-pic.jpeg" alt="" />
+
+            <h1>Mugeta Mugeta</h1>
+            <h3>Full Stack Software Engineer - MERN</h3>
+
+            <button>Email</button>
+            <button>LinkedIn</button>
+        </main>
+    )
+}
+
+export default PersonalInfo
