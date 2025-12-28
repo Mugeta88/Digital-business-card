5f2a17945eec690324b00d879f28020b2d03a5e4
Your Name <mugeta88@gmail.com>
2025-12-28 08:23:26 -0500

    added a card container with name, title, and email

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='card-container'>
+        <h1 className='name'>Mugeta Mugeta</h1>
+        <p className='title'>Full Stack Software Develper MERN</p>
+        <p className='email'>mugeta88@gmail.com</p>
+      </div>
+    </>
+  )
+}
+
+export default App
